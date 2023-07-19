cd24f0519a058564e800c834a7555981b94a7489
Michel Fotzo <fotzomichel.fmj@gmail.com>
2023-07-19 15:47:56 -0700

    Create the static library libmy.a

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/* Function to print a character to stdout */
+int _putchar(char c)
+{
+    return putchar(c); /* Use of a single return statement in the function */
+}
